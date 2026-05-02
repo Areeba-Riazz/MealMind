ae1060f2a2c55f1cb7bb55cf2c111fdda14b4b34
areeba-riazz <you@example.com>
2026-05-02 19:47:12 +0500

    feat: added PostHog for analytics and usertracking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext'
 
+import posthog from 'posthog-js'
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: 'https://app.posthog.com',
+  person_profiles: 'identified_only',
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
